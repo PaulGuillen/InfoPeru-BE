16cbd12e3767e42c2661f264fbee82531a65f710
Paul Guillen <paulguillen190@gmail.com>
2025-02-02 23:48:09 -0600

    Classic Login controller

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,7 +3,7 @@ const HTTP_STATUS_CODES = require("../utils/httpStatusCodes");
 const serviceAccount = require("../google_services_firebase.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount), 
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const auth = admin.auth();
@@ -11,24 +11,47 @@ const db = admin.firestore();
 
 module.exports = {
   async login(req, res, next) {
-    const { email} = req.body;
+    const { email, type, googleToken } = req.body;
 
     try {
-      const userRecord = await admin.auth().getUserByEmail(email);
-      const uid = userRecord.uid;
+      // Si el tipo de login es 'classic'
+      if (type === "classic") {
+        const userRecord = await admin.auth().getUserByEmail(email);
+        const uid = userRecord.uid;
 
-      res.status(HTTP_STATUS_CODES.OK).json({
-        status: HTTP_STATUS_CODES.OK,
-        message: "Inicio de sesión exitoso",
-        uid: uid,
+        return res.status(HTTP_STATUS_CODES.OK).json({
+          status: HTTP_STATUS_CODES.OK,
+          message: "Inicio de sesión exitoso",
+          uid: uid,
+        });
+      }
+
+      // Si el tipo de login es 'google', validamos el token de Google
+      if (type === "google" && googleToken) {
+        // Verificar el token de Google
+        const decodedToken = await admin.auth().verifyIdToken(googleToken);
+        const uid = decodedToken.uid;
+
+        // Puedes hacer más aquí como crear el usuario en Firestore si es necesario
+
+        return res.status(HTTP_STATUS_CODES.OK).json({
+          status: HTTP_STATUS_CODES.OK,
+          message: "Inicio de sesión con Google exitoso",
+          uid: uid,
+        });
+      }
+
+      // Si el tipo no es reconocido o falta el token de Google
+      return res.status(HTTP_STATUS_CODES.BAD_REQUEST).json({
+        status: HTTP_STATUS_CODES.BAD_REQUEST,
+        message: "Tipo de inicio de sesión no válido o token de Google faltante",
       });
     } catch (error) {
-      console.error("Error de inicio de sesión:", error);
-      res.status(HTTP_STATUS_CODES.NOT_FOUND).json({
-        status: HTTP_STATUS_CODES.NOT_FOUND,
-        message: "Credenciales de inicio de sesión incorrectas",
+      console.error("Error en el inicio de sesión:", error);
+      return res.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR).json({
+        status: HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR,
+        message: "Error en el inicio de sesión",
       });
     }
   },
-
 };
