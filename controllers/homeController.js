f62d29edcc3e44dd0a24be79d1d1d9f0a48e8bba
Paul Guillen <paulguillen190@gmail.com>
2025-06-14 00:02:18 -0500

    refactor getdollarQoute from newsController to optimized time response

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,33 +5,53 @@ const {
   HEADERS,
   COLLECTION_GRATITUDE,
   COLLECTION_SECTION,
+  COLLECTION_HOME_IMAGES,
 } = require("../utils/constants");
 
 const { db } = require("../utils/firebase");
 
 const getDollarQuote = async (_, res) => {
   try {
-    const response = await axios.get(EXTERNAL_APIS.DOLLAR_QUOTE, {
-      headers: {
-        "User-Agent": HEADERS.USER_AGENT,
-      },
-    });
+    
+    const [dollarRes, snapshot] = await Promise.all([
+      axios.get(EXTERNAL_APIS.DOLLAR_QUOTE, {
+        headers: {
+          "User-Agent": HEADERS.USER_AGENT,
+        },
+      }),
+      db.collection(COLLECTION_HOME_IMAGES).where("isDollarInfo", "==", true).limit(1).get(),
+    ]);
 
-    const data = response.data;
+    const dollarData = dollarRes.data;
+
+    let imageUrl = null;
+    let iconImage = null;
+    if (!snapshot.empty) {
+      const docData = snapshot.docs[0].data();
+      imageUrl = docData.imageUrl;
+      iconImage = docData.iconImage;
+    }
+
+    const enrichedData = {
+      ...dollarData,
+      imageUrl,
+      iconImage,
+    };
 
     return res.status(HTTP_STATUS_CODES.OK).json({
       status: HTTP_STATUS_CODES.OK,
       message: "Cotización obtenida exitosamente",
-      data: data,
+      data: enrichedData,
     });
   } catch (error) {
-    console.error("Error al obtener cotización desde DePeru:", error.message);
+    console.error("Error al obtener cotización o imagen:", error.message);
     return res.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR).json({
       status: HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR,
       message: "Error al obtener la cotización del dólar",
     });
   }
 };
+
 
 const getUit = async (_, res) => {
   try {
